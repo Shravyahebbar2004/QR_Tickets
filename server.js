179565ebb74d5a8f58b9cfae828ece321dfe5ea5
Shravya <shravyahebbar07@gmail.com>
2026-06-12 18:19:41 +0530

    Trigger Render Deploy

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS ? process.env.GMAIL_PASS.replace(/\s+/g, '') : ''
   }
+// Render auto-deploy trigger
 });
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
